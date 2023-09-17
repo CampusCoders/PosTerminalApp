101e0b518fdc6fdf88bd9500b7a6ac49fd2c8fa3
Hasan Ali Caliskan <hasanali7295@gmail.com>
2023-09-17 20:43:14 +0300

    Constants is added

---
+++ app/src/main/java/com/campuscoders/posterminalapp/utils/Constants.kt
@@ -0,0 +1,54 @@
+package com.campuscoders.posterminalapp.utils
+
+object Constants {
+    // MainUser & TerminalUser fields
+    const val TERMINAL_ID = "T0000352"
+    const val VKN_TCKN = "3250566851"
+    const val MEMBER_STORE = "TT00000000078"
+    const val PASSWORD = "00000000"
+    const val CELL_PHONE_NUMBER = "5010591596"
+    const val FIRST_NAME = "Pos"
+    const val LAST_NAME = "Manager"
+    const val IPTAL_IADE = true
+    const val TAHSILAT = true
+    const val KASIYER_GORUTULEME = true
+    const val KASIYER_EKLEME_DUZENLEME = true
+    const val KASIYER_SILME = true
+    const val URUN_GORUNTULEME = true
+    const val URUN_EKLEME_DUZENLEME = true
+    const val URUN_SILME = true
+    const val TUM_RAPORLARI_GORUNTULEME = true
+    const val RAPOR_KAYDET_GONDER = true
+    const val POS_YONETIMI = true
+    const val ADMIN = true
+
+    // Default Customer
+    const val CUSTOMER_VKN_TCKN = "51828901547"
+    const val CUSTOMER_COMPANY_NAME = ""
+    const val CUSTOMER_FIRST_NAME = "Nihai"
+    const val CUSTOMER_LAST_NAME = "Tüketici"
+    const val CUSTOMER_PHONE_NUMBER = ""
+    const val CUSTOMER_EMAIL = "customer@gmail.com"
+    const val CUSTOMER_PROVINCE = "İstanbul"
+    const val CUSTOMER_DISTRICT = "Zeytinburnu"
+    const val CUSTOMER_TAX_OFFICE = ""
+    const val CUSTOMER_ADDRESS = ""
+
+    const val ORDER_MALI_ID = "F222031000194400"
+    const val ORDER_ETTN = "3dht840nvb47228vhd39a920fjb949"
+    const val ORDER_NO_BACKEND = "3dht840nvb47228vhd39a920fjb949"
+
+    const val DATE_FORMAT = "yyyy-MM-dd"
+
+    const val TEST_PIN = "00000"
+    const val DURATION = 60000
+    const val MINUTE = DURATION / 60000
+    const val PROGRESS_BAR_DURATION = 1000
+
+    const val INFORMATIONS_VERIFYING = "Bilgileriniz Doğrulanıyor"
+    const val PIN_VERIFYING = "PIN Doğrulanıyor"
+    const val LOADING_MALI_ID = "Mali ID Alınıyor"
+    const val QUERY_SALE = "Satış Sorgulanıyor"
+    const val QUERY_DOCUMENT = "Belge Sorgulanıyor"
+    const val SHOPPING_CART = "Ürünler Yükleniyor"
+}
