e68ec68fc33a2b3e5f3e901f66709b82dc34f1ca
CAGRII <CAGRII@CAGRII-PC>
2023-10-01 16:45:12 +0300

    Password fragments (views) added

---
+++ app/src/main/java/com/campuscoders/posterminalapp/presentation/password/views/ForgetPasswordFragment.kt
@@ -0,0 +1,91 @@
+package com.campuscoders.posterminalapp.presentation.password.views
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import androidx.lifecycle.ViewModelProvider
+import com.campuscoders.posterminalapp.presentation.login.views.LoginTwoFragment
+import com.campuscoders.posterminalapp.R
+import com.campuscoders.posterminalapp.databinding.FragmentForgetPasswordBinding
+import com.campuscoders.posterminalapp.presentation.password.ForgetPasswordViewModel
+import com.campuscoders.posterminalapp.utils.Resource
+import com.campuscoders.posterminalapp.utils.hide
+import com.campuscoders.posterminalapp.utils.show
+import com.campuscoders.posterminalapp.utils.toast
+
+class ForgetPasswordFragment : Fragment() {
+
+    private var _binding: FragmentForgetPasswordBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var viewModel: ForgetPasswordViewModel
+
+    private var ftransaction: FragmentTransaction? = null
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding = FragmentForgetPasswordBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(requireActivity())[ForgetPasswordViewModel::class.java]
+        ftransaction = requireActivity().supportFragmentManager.beginTransaction()
+
+        binding.outlinedButtonCancel.setOnClickListener {
+            ftransaction?.let {
+                it.replace(R.id.fragmentContainerView, LoginTwoFragment())
+                it.commit()
+                // try popupbackstack
+            }
+        }
+        binding.outlinedButtonConfirm.setOnClickListener {
+            if (areFieldsNotEmpty()) {
+                viewModel.controlMainUserCellPhone(
+                    binding.textInputEditTextTCKN.text.toString(),
+                    binding.textInputEditTextPhone.text.toString())
+            }
+        }
+
+        observe()
+    }
+
+    private fun observe() {
+        viewModel.statusIsMatched.observe(viewLifecycleOwner) {
+            when(it) {
+                is Resource.Success -> {
+                    binding.progressBarForgetPassword.hide()
+                    toast(requireContext(),"MATCHED!",false)
+                    ftransaction?.let {f->
+                        f.replace(R.id.fragmentContainerView, ResetPasswordFragment())
+                        f.commit()
+                    }
+                }
+                is Resource.Loading -> {
+                    binding.progressBarForgetPassword.show()
+                }
+                is Resource.Error -> {
+                    binding.progressBarForgetPassword.hide()
+                    toast(requireContext(),it.message?:"ERROR!",false)
+                }
+            }
+        }
+    }
+
+    private fun areFieldsNotEmpty(): Boolean {
+        if (binding.textInputEditTextTCKN.text.toString() == "" || binding.textInputEditTextPhone.text.toString() == "") {
+            toast(requireContext(), "Lütfen, tüm alanları doldurun.", false)
+            return false
+        }
+        return true
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/campuscoders/posterminalapp/presentation/password/views/ResetPasswordFragment.kt
@@ -0,0 +1,118 @@
+package com.campuscoders.posterminalapp.presentation.password.views
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import androidx.lifecycle.ViewModelProvider
+import com.campuscoders.posterminalapp.R
+import com.campuscoders.posterminalapp.databinding.FragmentPasswordResetBinding
+import com.campuscoders.posterminalapp.presentation.login.views.LoginFragment
+import com.campuscoders.posterminalapp.presentation.password.ResetPasswordViewModel
+import com.campuscoders.posterminalapp.utils.Constants
+import com.campuscoders.posterminalapp.utils.Constants.CELL_PHONE_NUMBER
+import com.campuscoders.posterminalapp.utils.Resource
+import com.campuscoders.posterminalapp.utils.hide
+import com.campuscoders.posterminalapp.utils.show
+import com.campuscoders.posterminalapp.utils.toast
+
+class ResetPasswordFragment : Fragment() {
+
+    private var _binding: FragmentPasswordResetBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var viewModel: ResetPasswordViewModel
+
+    private var ftransaction: FragmentTransaction? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentPasswordResetBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(requireActivity())[ResetPasswordViewModel::class.java]
+        ftransaction = requireActivity().supportFragmentManager.beginTransaction()
+
+        binding.titleTextViewPasswordResetPhone.text = CELL_PHONE_NUMBER
+
+        binding.outlinedButtonConfirm.setOnClickListener {
+            if (areFieldsNotEmpty()) {
+                //viewModel.updateMainUserPassword(binding.textInputEditTextNewPassword.text.toString())
+                toast(requireContext(), "PASSWORD UPDATED!", false)
+            }
+        }
+
+        binding.outlinedButtonCancel.setOnClickListener {
+            // try popupbackstack
+        }
+
+        binding.textViewResendResetFragment.setOnClickListener {
+            viewModel.startTimer()
+        }
+
+        observe()
+    }
+
+    private fun observe() {
+        viewModel.statusIsUpdated.observe(viewLifecycleOwner) {
+            when (it) {
+                is Resource.Success -> {
+                    binding.progressBarResetPassword.hide()
+                    toast(requireContext(), "MainUser password is updated", false)
+                    ftransaction?.let { f ->
+                        f.replace(R.id.fragmentContainerView, LoginFragment())
+                        f.commit()
+                    }
+                }
+
+                is Resource.Loading -> {
+                    binding.progressBarResetPassword.show()
+                }
+
+                is Resource.Error -> {
+                    binding.progressBarResetPassword.hide()
+                    toast(requireContext(), it.message ?: "failed!", false)
+                }
+            }
+        }
+        viewModel.timer.observe(viewLifecycleOwner) {
+            binding.textViewResendResetFragment.visibility = View.INVISIBLE
+            binding.textViewCountDownTimerResetFragment.show()
+            binding.textViewCountDownTimerResetFragment.text = it ?: "null"
+        }
+        viewModel.onFinish.observe(viewLifecycleOwner) {
+            if (it) {
+                binding.textViewResendResetFragment.show()
+                binding.textViewCountDownTimerResetFragment.visibility = View.INVISIBLE
+            }
+        }
+    }
+
+    private fun areFieldsNotEmpty(): Boolean {
+        if (binding.textInputEditTextTemporaryPin.text.toString() == "" ||
+            binding.textInputEditTextNewPassword.text.toString() == "" ||
+            binding.textInputEditTextNewPasswordAgain.text.toString() == ""
+        ) {
+            toast(requireContext(), "Lütfen, tüm alanları doldurun", false)
+            return false
+        } else if (binding.textInputEditTextNewPassword.text.toString() != binding.textInputEditTextNewPasswordAgain.text.toString()) {
+            toast(requireContext(), "Parolalar eşleşmiyor", false)
+            return false
+        }
+        else if (binding.textInputEditTextTemporaryPin.text.toString() != Constants.TEST_PIN) {
+            toast(requireContext(), "Yanlış PIN", false)
+            return false
+        }
+        return true
+    }
+
+}
