1fb63f0cb76a27359e1887bd13020c896b563d84
Hasan Ali Caliskan <hasanali7295@gmail.com>
2023-10-09 21:35:25 +0300

    cancel_and_document package is refactored

--- app/src/main/java/com/campuscoders/posterminalapp/presentation/CancelSaleEDocumentActivity.kt
+++ app/src/main/java/com/campuscoders/posterminalapp/presentation/CancelSaleEDocumentActivity.kt
@@ -4,32 +4,35 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.FragmentTransaction
 import com.campuscoders.posterminalapp.R
-import com.campuscoders.posterminalapp.presentation.payment.views.DocumentQueryFragment
-import com.campuscoders.posterminalapp.presentation.payment.views.SaleCancelFragment
+import com.campuscoders.posterminalapp.presentation.cancel_and_document.views.CancelSaleAndQueryDocument
 
 class CancelSaleEDocumentActivity : AppCompatActivity() {
 
     private var ftransaction: FragmentTransaction? = null
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cancel_sale_edocument)
 
         ftransaction = supportFragmentManager.beginTransaction()
+
         val intent = intent
         val navigationValue = intent.getStringExtra("navigation")
-        if (navigationValue != null && navigationValue == "1") {
-            ftransaction?.let {
-                it.replace(R.id.fragmentContainerViewCancelSaleEDocument, SaleCancelFragment())
-                it.commit()
-            }
-        } else if (navigationValue != null && navigationValue == "2") {
-            ftransaction?.let {
-                it.replace(R.id.fragmentContainerViewCancelSaleEDocument, DocumentQueryFragment())
-                it.commit()
-            }
+
+        val cancelSaleAndQueryDocument = CancelSaleAndQueryDocument()
+        val bundle = Bundle()
+
+        if (navigationValue == "1") {
+            bundle.putString("from","cancel_sale")
+        } else {
+            bundle.putString("from","query_document")
         }
 
+        cancelSaleAndQueryDocument.arguments = bundle
+
+        ftransaction?.let {
+            it.replace(R.id.fragmentContainerViewCancelSaleEDocument, cancelSaleAndQueryDocument)
+            it.commit()
+        }
     }
 }
--- app/src/main/java/com/campuscoders/posterminalapp/presentation/cancel_and_document/views/CancelSaleAndQueryDocument.kt
+++ app/src/main/java/com/campuscoders/posterminalapp/presentation/cancel_and_document/views/CancelSaleAndQueryDocument.kt
@@ -1,6 +1,5 @@
-package com.campuscoders.posterminalapp.presentation.payment.views
+package com.campuscoders.posterminalapp.presentation.cancel_and_document.views
 
-import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -9,49 +8,42 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
-import com.campuscoders.posterminalapp.R
-import com.campuscoders.posterminalapp.databinding.FragmentCancelSaleBinding
+import com.campuscoders.posterminalapp.databinding.FragmentCancelSaleAndQueryDocumentBinding
 import com.campuscoders.posterminalapp.utils.Constants
 import com.campuscoders.posterminalapp.utils.showProgressDialog
 
-class SaleCancelFragment : Fragment() {
+class CancelSaleAndQueryDocument : Fragment() {
 
-    private var _binding: FragmentCancelSaleBinding? = null
+    private var _binding: FragmentCancelSaleAndQueryDocumentBinding? = null
     private val binding get() = _binding!!
+
     private var ftransaction: FragmentTransaction? = null
 
-    override fun onCreateView(
-
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        _binding = FragmentCancelSaleBinding.inflate(inflater, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding = FragmentCancelSaleAndQueryDocumentBinding.inflate(inflater, container, false)
         return binding.root
     }
 
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val Context = context
 
         ftransaction = requireActivity().supportFragmentManager.beginTransaction()
+
+        arguments?.let {
+            val from = it.getString("from")
+            if (from == "cancel_sale") {
+                binding.topAppBar.title = "Satış İptal"
+            } else {
+                binding.topAppBar.title = "E-Belge Sorgulama"
+            }
+        }
 
         binding.buttonQuerySale.setOnClickListener {
             context?.showProgressDialog(Constants.QUERY_SALE)
             Handler(Looper.getMainLooper()).postDelayed({
-                ftransaction?.let {
-                    it.replace(
-                        R.id.fragmentContainerViewCancelSaleEDocument,
-                        SaleCancelContentFragment()
-                    )
-                    it.addToBackStack(null)
-                    it.commit()
-                }
+                //
             }, Constants.PROGRESS_BAR_DURATION.toLong())
         }
-
-
 
     }
 
