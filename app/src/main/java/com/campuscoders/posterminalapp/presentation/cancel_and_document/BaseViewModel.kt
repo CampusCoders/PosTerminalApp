48ad92d7f7f6e436854c839279fb3296592f3c1c
Hasan Ali Caliskan <hasanali7295@gmail.com>
2023-10-14 23:50:38 +0300

    orderDetail live data is added on BaseViewModel

--- app/src/main/java/com/campuscoders/posterminalapp/presentation/cancel_and_document/BaseViewModel.kt
+++ app/src/main/java/com/campuscoders/posterminalapp/presentation/cancel_and_document/BaseViewModel.kt
@@ -1,10 +1,14 @@
 package com.campuscoders.posterminalapp.presentation.cancel_and_document
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.campuscoders.posterminalapp.domain.model.Orders
 import com.campuscoders.posterminalapp.domain.use_case.cancel_and_document.FetchOrderByMaliIdUseCase
 import com.campuscoders.posterminalapp.domain.use_case.cancel_and_document.FetchOrderByReceiptNoUseCase
 import com.campuscoders.posterminalapp.domain.use_case.cancel_and_document.FetchOrderByTerminalIdUseCase
+import com.campuscoders.posterminalapp.utils.Resource
 import kotlinx.coroutines.launch
 
 class BaseViewModel(
@@ -13,27 +17,34 @@ class BaseViewModel(
     private val fetchOrderByTerminalIdUseCase: FetchOrderByTerminalIdUseCase
 ): ViewModel() {
 
+    private var _statusOrderDetail = MutableLiveData<Resource<Orders>>()
+    val statusOrderDetail: LiveData<Resource<Orders>>
+        get() = _statusOrderDetail
+
     fun querySale(searchType: String, searchKey: String) {
-        // set live data value to loading
+        _statusOrderDetail.value = Resource.Loading(null)
         viewModelScope.launch {
             when(searchType) {
                 "receiptNo" -> {
                     val response = fetchOrderByReceiptNoUseCase.executeFetchOrderByReceiptNo(searchKey)
+                    _statusOrderDetail.value = response
                 }
                 "orderMaliId" -> {
                     val response = fetchOrderByMaliIdUseCase.executeFetchOrderByMaliId(searchKey)
+                    _statusOrderDetail.value = response
                 }
                 "terminalId" -> {
                     val response = fetchOrderByTerminalIdUseCase.executeFetchOrderByTerminal(searchKey)
+                    _statusOrderDetail.value = response
                 }
             }
         }
     }
 
     fun fetchLatestSuccessfulSale() {
-        // set live data value to loading
+        _statusOrderDetail.value = Resource.Loading(null)
         viewModelScope.launch {
-            //
+            // use case
         }
     }
 }
